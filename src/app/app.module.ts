d3e9a0efa2499e5d498737df50f135d3f9e822d1
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-03-13 15:38:31 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TileComponent } from './dashboard/components/tile/tile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    TileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

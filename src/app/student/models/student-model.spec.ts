4d17f9f2c8290422635ac5fb3c99aea6135acec9
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-03-14 10:49:30 +0100

    Magic Get/Set on Student model attributes

--- src/app/student/models/student-model.spec.ts
+++ src/app/student/models/student-model.spec.ts
@@ -4,4 +4,15 @@ describe('StudentModel', () => {
   it('should create an instance', () => {
     expect(new StudentModel()).toBeTruthy();
   });
+
+  it (`Should have 'Aubert' as lastName`, () => {
+    const student: StudentModel = new StudentModel()
+    // Fill student object
+    student.lastName = 'Aubert'
+    student.email = 'jean-luc.aubert@aelion.fr'
+    student.login = 'jlaubert'
+    student.password = 'toto'
+
+    expect(student.lastName).toBe('Aubert')
+  })
 });

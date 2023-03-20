be24389724dcb1b040bc95ee80e4ef1d2941a084
DaCodeManiaK <jla.webprojet@gmail.com>
2023-03-20 11:33:00 +0100

    Wiring Controller and Service

--- src/app/student/services/student.service.ts
+++ src/app/student/services/student.service.ts
@@ -39,7 +39,9 @@ export class StudentService {
 
   public findByLoginOrEmail(email: string, login: string): void {}
 
-  public add(student: IStudent): void {}
+  public add(student: IStudent): void {
+    console.log(`Controller send ${JSON.stringify(student)}`)
+  }
 
   public update(student: StudentModel): void {}
 

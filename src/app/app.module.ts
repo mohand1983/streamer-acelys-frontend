20d7c8f5f0f3332e21eee2d1ef4a2d0a7ffe492c
DaCodeManiaK <jla.webprojet@gmail.com>
2023-03-20 16:00:11 +0100

    Add CoreModule Core module expose managers and helpers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TileComponent } from './dashboard/components/tile/tile.component';
 import { StudentModule } from './student/student.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     StudentModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]

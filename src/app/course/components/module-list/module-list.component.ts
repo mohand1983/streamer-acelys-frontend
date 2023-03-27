80027300893a293a09e20c0205a2e24135122148
boudi1983 <boudi.mohand@gmail.com>
2023-03-27 22:32:51 +0200

    Modification in module list

--- src/app/course/components/module-list/module-list.component.ts
+++ src/app/course/components/module-list/module-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ModuleListType } from '../../types/module-list-type';
 import { ModuleType } from '../../types/module-type';
 
 @Component({
@@ -7,12 +8,19 @@ import { ModuleType } from '../../types/module-type';
   styleUrls: ['./module-list.component.scss']
 })
 export class ModuleListComponent implements OnInit {
+  @Input() public module!: ModuleListType
+  @Output() public onToggleCourse: EventEmitter<ModuleListType> = new EventEmitter()
 
   @Input() modules: ModuleType[] = []
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  public revealOrHide(module: ModuleListType): void {
+    module.isSelected = !module.isSelected
+    console.log(`Course was toggled : ${module.isSelected}`)
+    //this.onToggleModule.emit(module)
   }
 
 }
---
+++ src/app/course/types/module-list-type.ts
@@ -0,0 +1,4 @@
+import { ModuleType } from "./module-type";
+import { SelectedType } from "./selected-type";
+
+export type ModuleListType = ModuleType & SelectedType

3f384ec2fe3937ce942117912e2f49308d7372f0
boudi1983 <boudi.mohand@gmail.com>
2023-04-05 22:41:29 +0200

    User login

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 const users: Array<any> = [
   {
     login: 'user1',
@@ -16,15 +15,16 @@ const users: Array<any> = [
   providedIn: 'root'
 })
 export class UserService {
+
   private _user: any = undefined
 
   constructor(
   ) { }
 
-  public get user():any{
-    const jsonUser: string | null=sessionStorage.getItem('auth-key')
-    if(jsonUser!==null){
-      this._user=JSON.parse(jsonUser)
+  public get user(): any {
+    const jsonUser: string | null = sessionStorage.getItem('auth-key')
+    if (jsonUser !== null) {
+      this._user = JSON.parse(jsonUser)
     }
     return this._user
   }
@@ -36,14 +36,16 @@ export class UserService {
    */
 
   public authenticate(credentials: any): boolean {
-    this._user= users.find(
+    this._user = users.find(
       (user: any) =>
-      user.login === credentials.login && user.password === credentials.password)
+        user.login === credentials.login && user.password === credentials.password)
 
-      if(this._user){
-        sessionStorage.setItem('auth-key', JSON.stringify(credentials))
-      }
-      return this._user !== undefined
+    if (this._user) {
+      localStorage.setItem('auth-key', JSON.stringify(credentials))
+    }
+    return this._user !== undefined
   }
- 
+
+
+
 }
